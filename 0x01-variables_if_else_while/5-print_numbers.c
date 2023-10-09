53db5d0ba8b32e93aef3401e82115b0961ab25a3
michlopez2 <michellekem611@gmail.com>
2023-10-09 15:13:52 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,21 +4,21 @@
  * main - Entry point
  *
  * Description: Prints all single-digit numbers of base 10 starting from 0,
- *              followed by a new line.
+ * followed by a new line.
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-    int digit;
+	int digit;
 
-    for (digit = 0; digit <= 9; digit++)
-    {
-        putchar(digit + '0');
-    }
+	for (digit = 0; digit <= 9; digit++)
+	{
+		putchar(digit + '0');
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
 
